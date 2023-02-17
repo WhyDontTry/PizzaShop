c59638b6ef720c8adcfb6d27ec97b7e91425f857
rkudashev <kudashev.ravil@yandex.ru>
2023-02-17 13:26:53 +0500

    add index.erb

--- app.rb
+++ app.rb
@@ -8,5 +8,5 @@ require 'sqlite3'
 
 
 get '/' do
-    erb "Yo man! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"          
+    erb :index 
 end
