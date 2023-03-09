bb7a8d5648ac809d46ecf38c97ea01c1a02e822a
rkudashev <kudashev.ravil@yandex.ru>
2023-03-09 13:39:20 +0500

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -12,5 +12,8 @@ function something()
 
 function add_to_cart(id)
 {
-	alert('You added pizza with id: ' + id);
+	let key = 'product_' + id;
+	let x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
