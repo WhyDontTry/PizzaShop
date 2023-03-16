7e8e3dbabb9a3b1937c48c87a9ab9b047ae5c440
rkudashev <kudashev.ravil@yandex.ru>
2023-03-16 15:02:40 +0500

    display number of items in cart

--- public/app.js
+++ public/app.js
@@ -1,13 +1,7 @@
 
 function something()
 {
-	let x = window.localStorage.getItem('bbb');
-
-	x = x * 1 + 1; // фишка чтобы не было конкатенации строк
-
-	window.localStorage.setItem('bbb', x);
-	
-	alert(x);
+	alert('Items in your cart: ' + cart_get_number_of_items());
 }
 
 function add_to_cart(id)
@@ -16,4 +10,19 @@ function add_to_cart(id)
 	let x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+}
+
+function cart_get_number_of_items()
+{
+	let items = 0;
+
+	for (let i = 0; i < window.localStorage.length; i++)
+	{
+		const key = window.localStorage.key(i);
+		if ( key.indexOf('product_') == 0 ) {
+			items += Number(window.localStorage.getItem(key));
+		}
+	}
+	
+	return items;
 }
